ed0d5458c19750ac013bc5696538c18ddd1998c2
michael <michael.catchen@colorado.edu>
2019-04-11 07:54:59 -0600

    adding bucky to pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,9 @@
 
 import sys, os, subprocess
 
-subprocess.call(["mkdir", "tmp"])
+input_dir = 'mito_genes'
+dir_name= 'mito_genes_only'
+subprocess.call(["mkdir",  dir_name])
 
 
 print("Setting up MrBayes...\n\n\n")
@@ -14,12 +16,17 @@ subprocess.call("cp ./src/mb ../../scripts", shell=True)
 os.chdir("../..")
 print os.getcwd()
 
+print("Setting up Bucky")
+
 
 print("Zipping Nexus File...\n\n\ns")
 subprocess.call("ls")
-subprocess.call("tar czf ./tmp/genes.tar.gz ./nexus/*.nexus", shell=True)
+subprocess.call("tar czf ./" + dir_name + "/genes.tar.gz ./" + input_dir +"/*.nexus", shell=True)
 
 print os.getcwd()
 print("Running MrBayes...\n\n\n")
 os.chdir("./scripts")
-subprocess.call("perl mb.pl ../tmp/genes.tar.gz -m ./mb_block.txt -o ../tmp/mb_data", shell=True)
+subprocess.call("perl mb.pl ../" + dir_name + "/genes.tar.gz -m ./mb_block.txt -o ../" + dir_name + "/mb_data", shell=True)
+
+printf("Running Bucky...\n\n\n\n")
+subprocess.call("perl bucky.pl ../" + dir_name + "/mb_data/genes.mb.tar -o ../" + dir_name + "/bucky_output", shell=True)
